91488df7cb52a1205c974c57f8a04a41cbabf33f
hyosun <andy703744@gmail.com>
2020-05-15 19:19:30 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from "react";
-// import Potato from "./Potato";
-// import PropTypes from "prop-types";
+import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component{
 
-  state = {
-    isLoading : true
+  /* 버튼에는 기본적으로 onClick을 가지고 있다. */
+
+  /* React.Component가 가지고 있는 object 중 하나
+   직접 변경 X, setState로 값을 변경한다.*/
+  state = { 
+    isLoading : true,
+    movies:[]
   };
 
-  componentWillMount(){
-    setTimeout(() =>{
-      this.setState({isLoading:false});
-    }, 6000);
+  // async function() 기다리게 하는 메서드, async를 써야 await 사용 가능
+  getMovies = async() =>{
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json"); 
+  }
+
+  componentDidMount(){
+    this.getMovies()
   }
 
     render(){
       const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>
+    return <div>{isLoading ? "Loading.." : "We are ready"}</div>
     }
+    
 }
-  
+
 export default App;
--- src/Potato.js
+++ src/Potato.js
@@ -1,5 +1,4 @@
 import React from "react"; //import React 시켜주기
-
 function Potato(){
     return <h3>I love you</h3>
 }
