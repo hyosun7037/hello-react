e110ff529e5a70c538e9a8b1505abbdbef01c24b
hyosun <andy703744@gmail.com>
2020-05-26 18:39:12 +0900

    css 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-
-
+import Movie from './Movie';
+import "./App.css";
 class App extends React.Component{
+
 
   /* 버튼에는 기본적으로 onClick을 가지고 있다. */
 
@@ -14,19 +15,46 @@ class App extends React.Component{
   };
 
   // async function() 기다리게 하는 메서드, async를 써야 await 사용 가능
+  
   getMovies = async() =>{
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json"); 
-  }
+    const {
+      data:{
+        data:{movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading:false})
+  };
 
   componentDidMount(){
-    this.getMovies()
+    this.getMovies();
   }
 
     render(){
-      const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading.." : "We are ready"}</div>
+      const { isLoading, movies } = this.state;
+    return (
+             <section className="container">
+               {isLoading  ? (
+               <div className="loader">
+                <span className="loader__text">Loading..</span>
+               </div>
+               ) : (
+                 <div className="movies">
+                   {movies.map(movie =>(
+              <Movie 
+              key={movie.id}
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+              />  
+         ))}
+           </div>
+               )}
+         </section>
+          );
     }
-    
 }
 
 export default App;
