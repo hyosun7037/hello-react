38e63ba75a3edc569c0b94c71b1fc01aa9a02f99
hyosun <andy703744@gmail.com>
2020-05-28 18:19:00 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import "./App.css";
-class App extends React.Component{
 
-
-  /* 버튼에는 기본적으로 onClick을 가지고 있다. */
-
-  /* React.Component가 가지고 있는 object 중 하나
-   직접 변경 X, setState로 값을 변경한다.*/
-  state = { 
-    isLoading : true,
-    movies:[]
-  };
-
-  // async function() 기다리게 하는 메서드, async를 써야 await 사용 가능
-  
-  getMovies = async() =>{
-    const {
-      data:{
-        data:{movies}
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading:false})
-  };
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-    render(){
-      const { isLoading, movies } = this.state;
-    return (
-             <section className="container">
-               {isLoading  ? (
-               <div className="loader">
-                <span className="loader__text">Loading..</span>
-               </div>
-               ) : (
-                 <div className="movies">
-                   {movies.map(movie =>(
-              <Movie 
-              key={movie.id}
-              id={movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              summary={movie.summary} 
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-              />  
-         ))}
-           </div>
-               )}
-         </section>
-          );
-    }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
